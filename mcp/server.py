f38a57b30c65d90558305469bea19a7365e3e7d0
hyeongmin <nsby8882@naver.com>
2026-03-11 15:47:16 +0900

    feat: add Reranker Tool in MCP - cohere reranker API - 무료 플랜 가정

--- mcp/server.py
+++ mcp/server.py
@@ -13,7 +13,6 @@ load_dotenv(Path(__file__).parent.parent / ".env")
 from mcp.server.fastmcp import FastMCP
 from data_pipeline.storage import query_hybrid
 
-
 mcp = FastMCP("spring_docs")
 
 @mcp.tool()
@@ -42,7 +41,7 @@ def get_docs(query: str, category: str = None) -> str:
     Returns:
         List of documents.
     """
-    docs = query_hybrid(query, k=5, category=category)
+    docs = query_hybrid(query, k=5, category=category, use_reranker=False)
     results = [
         {
             "page_content": doc.page_content,
@@ -51,6 +50,44 @@ def get_docs(query: str, category: str = None) -> str:
         for doc in docs
     ]
     return json.dumps(results, ensure_ascii=False)
+
+@mcp.tool()
+def get_docs_with_reranker(query: str, category: str = None) -> str:
+    """
+    Get documents for a query.
+    Supported documentation categories:
+    - spring boot reference documentation
+    - spring data redis reference documentation
+    - spring data jpa reference documentation
+    - spring security reference documentation
+    - spring cloud gateway reference documentation
+
+    category can be one of the following:
+    - spring-boot
+    - spring-data-redis
+    - spring-data-jpa
+    - spring-security
+    - spring-cloud-gateway
+    - None (default : all categories)
+    
+    use english for query
+    10 request limit per minute (Api free plan)
+    
+    Args:
+        query: The search query string.
+    Returns:
+        List of documents.
+    """
+    docs = query_hybrid(query, k=5, category=category, use_reranker=True)
+    results = [
+        {
+            "page_content": doc.page_content,
+            "metadata": doc.metadata,
+        }
+        for doc in docs
+    ]
+    return json.dumps(results, ensure_ascii=False)
+
 
 if __name__ == "__main__":
     mcp.run()
